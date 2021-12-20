f073adead82d4571f7e63d42f90d48f89109050c
Dmitry Avgustis <davgustis@gem.cz>
2021-12-20 15:49:00 +0100

    local config

--- src/main/java/com/augy/azuredemo/AzureDemoApplication.java
+++ src/main/java/com/augy/azuredemo/AzureDemoApplication.java
@@ -1,31 +1,51 @@
 package com.augy.azuredemo;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
-
-import java.time.Duration;
 
 @SpringBootApplication
 
 public class AzureDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AzureDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AzureDemoApplication.class, args);
+    }
+
+}
+
+@Data
+class TestEntity {
+    private Long id;
+    private String name;
+}
+
+interface MyRepo extends ReactiveCrudRepository<TestEntity, Long> {
 
 }
 
 @RestController
 @Slf4j
 class MyController {
-	@GetMapping
-	Mono<String> hello() {
-		Mono<String> hello_azure = Mono.just("Hello azure").delayElement(Duration.ofSeconds(5));
-		System.out.println("Test");
-		return hello_azure;
-	}
+    @Autowired
+    MyRepo myRepo;
+    @Value("${my.secret}")
+    private String secret;
+
+    @GetMapping
+    Mono<TestEntity> hello() {
+        return myRepo.findAll().last();
+    }
+
+    @GetMapping("/secret")
+    public Mono<String> secret() {
+        return Mono.just(secret);
+    }
 }
